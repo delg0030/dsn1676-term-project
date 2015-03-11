909ab10b6eeaa8ccc6d9b093f09732b7e0ce97a7
Brittany <delgatyb@gmail.com>
2015-03-11 09:55:46 -0400

    additional buttons made

--- js/main.js
+++ js/main.js
@@ -4,11 +4,27 @@ var $box = $('.box');
 var $btnCollapse = $('.btn-collapse-expand');
 var $panel = $('.panel');
 
+var $btnCollapsetwo = $('.btn-collapse-expandtwo');
+var $paneltwo = $('.paneltwo');
+
+var $btnCollapsethree = $('.btn-collapse-expandthree');
+var $panelthree = $('.panelthree');
+
 $btnShow.on('click', function () {
     $box.toggleClass('js-box-show');
 });
 
 $btnCollapse.on('click', function () {
     $panel.toggleClass('js-panel-collapse')
+    
+});
+
+$btnCollapsetwo.on('click', function () {
+    $paneltwo.toggleClass('js-panel-collapsetwo')
+    
+});
+
+$btnCollapsethree.on('click', function () {
+    $panelthree.toggleClass('js-panel-collapsethree')
     
 });
